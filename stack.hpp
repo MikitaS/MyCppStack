a530175cf133e76aec176498cb25ae2d6fd5deb3
Mikita <nikitastrizhak@mail.ru>
2018-12-01 20:43:09 +0300

    add coments to stack interface

--- stack.hpp
+++ stack.hpp
@@ -21,14 +21,26 @@ class Stack : public StackElement
 {
 public:
 	Stack();
+	
+	/* creates copy of given stack*/
 	Stack(Stack const & stack);
+	
+	/* destructs stack and all alocated memory*/
 	~Stack();
 	
-	
+	/* return last element and destructs it*/
 	int pop();
+	
+	/*creates new stack element*/
 	void push(int elem);
+	
+	/*returns current size of stack*/
 	size_t size() const;
+	
+	/*returns true if stack is empty*/ 
 	bool is_empty() const;
+	
+	/*copyes given stack to current*/
 	Stack & operator = (Stack const & stack);
 	
 private:
